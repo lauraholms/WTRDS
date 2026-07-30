eedfa62c7558bda775fbec12edf161f02b9d1eec
Laura <lauraholms@proton.me>
2026-07-30 21:30:43 +0300

    Add SchedulerConfig class with configuration constants

---
+++ src/main/java/scheduler/config/SchedulerConfig.java
@@ -0,0 +1,17 @@
+package scheduler.config;
+
+public final class SchedulerConfig {
+
+    public static final int DEFAULT_DURATION = 20;
+
+    public static final int MAX_DAILY_WATER = 1500;
+
+    public static final int RAIN_SKIP_MM = 5;
+
+    public static final String DEFAULT_TIME =
+            "06:00";
+
+    private SchedulerConfig() {
+    }
+
+}
